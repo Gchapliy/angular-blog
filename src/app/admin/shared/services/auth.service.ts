de2aaf55a94da656404d9d3d4b41825b2c3e3d60
Gchapliy <grishachapliy1@gmail.com>
2020-09-15 17:32:57 +0300

    token storage

--- src/app/admin/shared/services/auth.service.ts
+++ src/app/admin/shared/services/auth.service.ts
@@ -13,25 +13,30 @@ export class AuthService {
   }
 
   get token(): string {
-    return '';
-  }
+    const expDate = new Date(localStorage.getItem('fb-token-exp'));
+    if (new Date() > expDate) {
+      this.logout();
+      return null;
+    }
 
-  login(user: User): Observable<any> {
-    return this.http.post(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${environment.apiKey}`, user)
-      .pipe(
-        tap(this.setToken)
-      );
+    return localStorage.getItem('fb-token');
   }
 
   logout(): void {
-
+    this.setToken(null);
   }
 
   isAuthenticated(): boolean {
     return !!this.token;
   }
 
-  private setToken(response: FbAuthResponse) {
-
+  private setToken(response: FbAuthResponse | null) {
+    if (response) {
+      const expDate = new Date(new Date().getTime() + +response.expiresIn * 1000);
+      localStorage.setItem('fb-token', response.idToken);
+      localStorage.setItem('fb-token-exp', expDate.toString());
+    } else {
+      localStorage.clear();
+    }
   }
 }
