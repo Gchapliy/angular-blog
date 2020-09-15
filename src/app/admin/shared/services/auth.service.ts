523eaa42e6e05d2a7e6fc36013ce4c56b024a2fa
Gchapliy <grishachapliy1@gmail.com>
2020-09-15 22:33:17 +0300

    errors handling

--- src/app/admin/shared/services/auth.service.ts
+++ src/app/admin/shared/services/auth.service.ts
@@ -1,13 +1,15 @@
 import {Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpErrorResponse} from '@angular/common/http';
 import {FbAuthResponse, User} from '../../../shared/interfaces';
-import {Observable} from 'rxjs';
+import {Observable, Subject, throwError} from 'rxjs';
 import {environment} from '../../../../environments/environment';
-import {tap} from 'rxjs/operators';
+import {catchError, tap} from 'rxjs/operators';
 
 // {providedIn: 'root'} needs only when service must be in root module
 @Injectable()
 export class AuthService {
+  public error$: Subject<string> = new Subject<string>();
+
   constructor(private http: HttpClient) {
 
   }
@@ -22,12 +24,39 @@ export class AuthService {
     return localStorage.getItem('fb-token');
   }
 
+  login(user: User): Observable<any> {
+    user.returnSecureToken = true
+    return this.http.post(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${environment.apiKey}`, user)
+      .pipe(
+        tap(this.setToken),
+        catchError(this.handleError.bind(this))
+      )
+  }
+
   logout(): void {
     this.setToken(null);
   }
 
   isAuthenticated(): boolean {
     return !!this.token;
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    const {message} = error.error.error
+
+    switch (message) {
+      case 'INVALID_EMAIL':
+        this.error$.next("Wrong email");
+        break;
+      case 'INVALID_PASSWORD':
+        this.error$.next("Wrong password");
+        break;
+      case 'EMAIL_NOT_FOUND':
+        this.error$.next("Email not found");
+        break;
+    }
+
+    return throwError(error)
   }
 
   private setToken(response: FbAuthResponse | null) {
