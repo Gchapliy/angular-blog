3191d45758a514dfd14584bd583ebdf037ca3adc
Gchapliy <grishachapliy1@gmail.com>
2020-09-16 07:32:43 +0300

    post creating in progress

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,6 @@ import {Environment} from './interface';
 
 export const environment: Environment = {
   production: true,
-  apiKey: 'AIzaSyDmCWdXTvTjIj8wOQKwi7rd7-nSSdMYVqE'
+  apiKey: 'AIzaSyDmCWdXTvTjIj8wOQKwi7rd7-nSSdMYVqE',
+  fbDbUrl: 'https://angular-blog-5da2a.firebaseio.com/'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,8 @@ import {Environment} from './interface';
 
 export const environment: Environment = {
   production: false,
-  apiKey: 'AIzaSyDmCWdXTvTjIj8wOQKwi7rd7-nSSdMYVqE'
+  apiKey: 'AIzaSyDmCWdXTvTjIj8wOQKwi7rd7-nSSdMYVqE',
+  fbDbUrl: 'https://angular-blog-5da2a.firebaseio.com/'
 };
 
 /*
