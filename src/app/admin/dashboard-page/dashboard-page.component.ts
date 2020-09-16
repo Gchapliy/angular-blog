0a27f1d63c057634c5712b8c69c36a176488b6a5
Hryhorii <grishachapliy1@gmail.com>
2020-09-16 12:59:24 +0300

    removing post

--- src/app/admin/dashboard-page/dashboard-page.component.ts
+++ src/app/admin/dashboard-page/dashboard-page.component.ts
@@ -1,7 +1,7 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
 import {PostsService} from '../../shared/posts.service';
 import {Post} from '../../shared/interfaces';
-import {pipe, Subscription} from 'rxjs';
+import {Observable, pipe, Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-dashboard-page',
@@ -12,6 +12,7 @@ export class DashboardPageComponent implements OnInit, OnDestroy {
 
   posts: Post[] = [];
   pSub: Subscription;
+  dSub: Subscription;
   searchStr = '';
 
   constructor(private postsService: PostsService) {
@@ -23,13 +24,23 @@ export class DashboardPageComponent implements OnInit, OnDestroy {
     });
   }
 
+  remove(id: string): void {
+    this.dSub = this.postsService.remove(id).subscribe(() => {
+      this.posts = this.posts.filter(post => post.id !== id);
+
+      if (this.posts.length === 0) {
+        this.posts = undefined;
+      }
+    });
+  }
+
   ngOnDestroy(): void {
     if (this.pSub) {
       this.pSub.unsubscribe();
     }
-  }
 
-  remove(id: string) {
-
+    if (this.dSub) {
+      this.dSub.unsubscribe();
+    }
   }
 }
